4fa682fe241fc341770a67967641cd982335fa74
JoanaMota <joanacarvalhomota@ua.pt>
2021-09-22 22:55:25 +0100

    lists

--- TheStandardTemplateLibrarySTL/4_lists.cpp
+++ TheStandardTemplateLibrarySTL/4_lists.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <list>
 
+void print(std::list<int> f_list)
+{
+    for (int element : f_list)
+    {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
     std::list<int> numbers;
@@ -8,22 +17,25 @@ int main()
     numbers.push_back(2);
     numbers.push_back(3);
     numbers.push_front(0);
+    print(numbers);
 
     std::list<int>::iterator it = numbers.begin();
     it++;
     numbers.insert(it, 100);
     std::cout << "Element: " << *it << std::endl;
+    print(numbers);
 
     std::list<int>::iterator eraseIt = numbers.begin();
     eraseIt++;
     eraseIt = numbers.erase(eraseIt);
-    std::cout << "Element: " << *eraseIt << std::endl;
+    std::cout << "Next Element: " << *eraseIt << std::endl;
+    print(numbers);
 
     for (std::list<int>::iterator it = numbers.begin(); it != numbers.end();)
     {
         if (*it == 2)
         {
-            numbers.insert(it, 1234);
+            numbers.insert(it, 5555);
         }
         if (*it == 1)
         {
@@ -38,11 +50,9 @@ int main()
             it++;
         }
     }
-
-    for (std::list<int>::iterator it = numbers.begin(); it != numbers.end(); it++)
-    {
-        std::cout << *it << std::endl;
-    }
+    print(numbers);
+    numbers.sort();
+    print(numbers);
 
     return 0;
 }
